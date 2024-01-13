6583257414789deb4e2750653a7ce5674316d627
Caio Padilha <ccfpadilha@hotmail.com>
2024-01-13 17:55:37 -0300

    Creating proto files and preparing gRPC server

---
+++ pkg/service/token_service.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"context"
+	"github.com/ccesarfp/shrine/pkg/protobuf"
+)
+
+type Server struct {
+	protobuf.UnimplementedTokenServer
+}
+
+func (s *Server) CreateToken(ctx context.Context, in *protobuf.UserRequest) (*protobuf.TokenResponse, error) {
+	return &protobuf.TokenResponse{
+		Status: "ok",
+		Token:  "aaaaa",
+	}, nil
+}
